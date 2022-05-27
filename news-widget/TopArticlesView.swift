9200a99bf362a83d5a47e050f1260598a6371cee
Bennett Perkins <bennettperkins@gmail.com>
2022-05-27 14:26:50 +0800

    fixes

--- news-widget/TopArticlesView.swift
+++ news-widget/TopArticlesView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct TopArticlesViewController: UIViewController {
+struct TopArticlesView: View {
     var news: [URL]
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
